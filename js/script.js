f7b5e43bdebfd6654cc6614cb31181c7aef7fb9d
lodisaq <lodisaq@163.com>
2023-10-20 18:30:54 +0800

    Site updated: 2023-10-20 18:30:51

--- js/script.js
+++ js/script.js
@@ -54,43 +54,47 @@ function owoBig() {
     observer.observe(document.getElementById('post-comment'), { subtree: true, childList: true }) // 监听的 元素 和 配置项
 }
 
-// 复制提醒
-document.addEventListener("copy", function () {
-  debounce(function () {
-    new Vue({
-      data: function () {
-        this.$notify({
-          title: "哎嘿！复制成功🍬",
-          message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
-          position: 'top-left',
-          offset: 50,
-          showClose: true,
-          type: "success",
-          duration: 5000
-        });
-      }
-    })
-  }, 300);
-})
+let TT = null;
+function debounce(e, t) {
+    null !== TT && clearTimeout(TT),
+    TT = setTimeout(e, t)
+}
 
-
-// f12提醒但不禁用
-document.onkeydown = function (e) {
-  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
-    debounce(function () {
-      new Vue({
-        data: function () {
-          this.$notify({
-            title: "你已被发现😜",
-            message: "小伙子，扒源记住要遵循GPL协议！",
-            position: 'top-left',
-            offset: 50,
-            showClose: true,
-            type: "warning",
-            duration: 5000
-          });
-        }
-      })
-    }, 300);
-  }
-};
+if (document.addEventListener("copy", (function() {
+    debounce((function() {
+        new Vue({
+            data: function() {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+                    position: "top-left",
+                    offset: 50,
+                    showClose: !0,
+                    type: "success",
+                    duration: 5e3
+                })
+            }
+        })
+    }
+    ), 300)
+}
+)),
+document.onkeydown = function(e) {
+    (123 == e.keyCode || e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode) || e.ctrlKey && 85 === e.keyCode) && debounce((function() {
+        new Vue({
+            data: function() {
+                this.$notify({
+                    title: "你已被发现😜",
+                    message: "小伙子，扒源记住要遵循GPL协议！",
+                    position: "top-left",
+                    offset: 50,
+                    showClose: !0,
+                    type: "warning",
+                    duration: 5e3
+                })
+            }
+        })
+    }
+    ), 300)
+}
+){}
